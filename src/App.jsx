42b6245b446fa4cfe54314cdddbbf7c77cb6b7e6
Exequiel Lobaiza <exelob25@gmail.com>
2024-11-28 13:29:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default App
