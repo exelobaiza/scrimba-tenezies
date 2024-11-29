49ee387d78ed6f89f8d5e17002e83911b1b266e3
Exequiel Lobaiza <exelob25@gmail.com>
2024-11-29 13:42:49 +0100

    adding styles and behaviors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-
+import Die from './components/die'
 function App() {
   return (
     <div className="app-container">
       <div>
+        <h1>Tenzies</h1>
+        <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className='dice-container'>
+          <Die />
+        </div>
+        <button className='roll-btn'>Roll</button>
       </div>
     </div>
   )
---
+++ src/components/Die.jsx
@@ -0,0 +1,16 @@
+export default function Die(props) {
+  return (
+    <>
+    <button>{props.value}1</button>
+    <button>{props.value}2</button>
+    <button>{props.value}3</button>
+    <button>{props.value}4</button>
+    <button>{props.value}5</button>
+    <button>{props.value}6</button>
+    <button>{props.value}7</button>
+    <button>{props.value}8</button>
+    <button>{props.value}9</button>
+    <button>{props.value}10</button>
+    </>
+  );
+}
