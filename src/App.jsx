a5be2e21197549d4f184445093e9d6b3d2354d13
Exequiel Lobaiza <exelob25@gmail.com>
2024-11-29 14:37:50 +0100

    adding element

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,34 @@ import './App.css'
 import Die from './components/die'
 function App() {
 
+  /**
+ * Challenge:
+ * 
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `generateAllNewDice` function so it 
+ * loads all new dice as soon as the app loads)
+ * 
+ * Map over the state numbers array to generate our array
+ * of Die components and render those in place of our
+ * manually-written 10 Die elements.
+ */
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
   }
-  console.log(generateAllNewDice());
-
+  const [dice, setDice] = useState(generateAllNewDice());
+  
+  const diceElements = dice.map((number) => {
+    return <Die value={number} />
+  })
+  
   return (
     <div className="app-container">
       <div>
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='dice-container'>
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-          <Die value={5} />
-          <Die value={6} />
-          <Die value={7} />
-          <Die value={8} />
-          <Die value={9} />
-          <Die value={10} />
+          {diceElements}
         </div>
         <button className='roll-btn'>Roll</button>
       </div>
